debef6200ac97bfca80153d97d0032910536529c
mathematiCode <160360849+mathematiCode@users.noreply.github.com>
2024-07-12 22:36:27 -0500

    Added the circle/bar toggle to the basic page

--- mathVisual.js
+++ mathVisual.js
@@ -1,258 +1,309 @@
-const  mathVisual = {};
+const mathVisual = {};
 
-mathVisual.fractionBar = (canvasElement, numerator, denominator, lineThickness=5, colorFill="rgb(120, 190, 250)") => {
+mathVisual.fractionBar = (
+  canvasElement,
+  numerator,
+  denominator,
+  lineThickness = 5,
+  colorFill = "rgb(120, 190, 250)"
+) => {
+  let canvas = canvasElement.getContext("2d");
+  const width = canvasElement.width;
+  const height = canvasElement.height;
 
-    let canvas = canvasElement.getContext("2d");
-    const width = canvasElement.width;
-    const height = canvasElement.height;
+  if (numerator > denominator) {
+    return alert(
+      `Please enter a proper fraction. num=${numerator} denom=${denominator} ${
+        numerator > denominator
+      }`
+    );
+  }
+  canvas.clearRect(0, 0, width, height);
+  const interval = width / denominator;
+  let separator = interval; // Need two variables becuase separator will increment while interval stays constant.
 
-    if (numerator > denominator) {
-        return alert(
-            `Please enter a proper fraction. num=${numerator} denom=${denominator} ${
-                numerator > denominator
-            }`
-        );
+  let shaded = interval * numerator;
+  canvas.fillStyle = "white";
+  canvas.fillRect(
+    lineThickness,
+    lineThickness,
+    width - lineThickness * 2,
+    height - lineThickness * 2
+  );
+  canvas.fillStyle = colorFill;
+  canvas.fillRect(
+    lineThickness,
+    lineThickness,
+    shaded - lineThickness,
+    height - lineThickness
+  );
+
+  for (let i = 1; i < denominator; i++) {
+    canvas.beginPath();
+    canvas.moveTo(separator, 0);
+    canvas.lineTo(separator, height);
+    canvas.closePath();
+    canvas.lineWidth = lineThickness;
+    canvas.stroke();
+    separator = separator + interval;
+  }
+  canvas.lineWidth = lineThickness * 2;
+  canvas.fillStyle = "black";
+  canvas.strokeRect(0, 0, width, height);
+};
+
+mathVisual.fractionCircle = (
+  canvasElement,
+  numerator,
+  denominator,
+  lineThickness = 5,
+  colorFill = "rgb(120, 190, 250)"
+) => {
+  const width = canvasElement.width;
+  const height = canvasElement.height;
+  if (numerator < 0 || denominator < 0) {
+    return alert(`Please enter a positive numerator and denominator.`);
+  }
+
+  if (numerator > denominator) {
+    return alert(
+      `Please enter a proper fraction. num=${numerator} denom=${denominator} ${
+        numerator > denominator
+      }`
+    );
+  }
+
+  let canvas = canvasElement.getContext("2d");
+  canvas.clearRect(0, 0, width, height);
+
+  // Set the circle properties
+  const x = width / 2; // x-coordinate of the center
+  const y = height / 2; // y-coordinate of the center
+  let radius = 0;
+  if (width <= height) {
+    radius = width / 2 - 10;
+  } else {
+    radius = height / 2 - 10;
+  }
+  const startAngle = 0; // starting angle (in radians)
+  const endAngle = 2 * Math.PI; // ending angle (in radians)
+
+  const angle = (Math.PI * 2) / denominator;
+  let currentAngle = angle;
+  let shaded = angle * numerator;
+
+  canvas.beginPath();
+  canvas.lineWidth = lineThickness;
+  canvas.arc(x, y, radius, startAngle, endAngle); // Drawing the outer circle
+  canvas.fillStyle = "white";
+  canvas.fill();
+  canvas.lineTo(x, y);
+  canvas.stroke();
+
+  canvas.beginPath(); // Filling in the shaded pieces
+  canvas.arc(x, y, radius, startAngle, shaded);
+  canvas.lineTo(x, y);
+  // I've only drawn an arc and one line, but when the fill() method is called, the canvas will automatically draw a line back to the starting point. So I do not need to explicitly create the 3rd line to close the path to be filled.
+  canvas.fillStyle = colorFill;
+  canvas.fill();
+  canvas.stroke();
+
+  canvas.beginPath();
+  for (i = 0; i < denominator; i++) {
+    // Creating lines for each slice
+    canvas.arc(x, y, radius, currentAngle, currentAngle + angle);
+    canvas.lineTo(x, y);
+    currentAngle = currentAngle + angle;
+  }
+  canvas.stroke();
+};
+
+mathVisual.fractionMultiplication = (
+  canvasElement,
+  wholeNum1,
+  numerator1,
+  denominator1,
+  wholeNum2,
+  numerator2,
+  denominator2,
+  style = "area"
+) => {
+  let canvas = canvasElement.getContext("2d");
+  const width = canvasElement.width;
+  const height = canvasElement.height;
+  canvas.clearRect(0, 0, width, height);
+
+  if (wholeNum1 === 0 && wholeNum2 === 0) {
+  }
+};
+
+mathVisual.fractionDivision = (
+  canvasElement,
+  wholeNum1,
+  numerator1,
+  denominator1,
+  numerator2,
+  denominator2,
+  lineThickness = 4,
+  colorFill = "#52a4b0",
+  colorFill2 = "#f0a68c",
+  style = "bar"
+) => {
+  let canvas = canvasElement.getContext("2d");
+  const width = canvasElement.width;
+  const spaceForLabels = 50;
+  const height = canvasElement.height - spaceForLabels;
+  canvas.clearRect(0, 0, width, height + spaceForLabels);
+  let numSections = 0; //
+
+  // Need to add error handling for decimal, blank or negative inputs
+
+  if (!wholeNum1) {
+    wholeNum1 = 0;
+  } // If the wholeNum input is blank, the variable is set to 0.
+
+  if (
+    wholeNum1 < 0 ||
+    numerator1 < 0 ||
+    denominator1 < 0 ||
+    numerator2 < 0 ||
+    denominator2 < 0
+  ) {
+    return alert(
+      `Please enter a positive number in each box or leave it blank.`
+    );
+  } else if (wholeNum1 == 0 && numerator1 == 0) {
+    return alert(`Please enter a positive dividend.`);
+  } else if (denominator1 == 0 || denominator2 == 0) {
+    return alert(`All denominators must be positive.`);
+  } else if (wholeNum1 == 0) {
+    numSections = denominator1;
+  } else if (numerator1 == 0) {
+    numSections = wholeNum1;
+  } else {
+    numSections = wholeNum1 + 1;
+  }
+
+  const interval = width / numSections;
+  let separator = interval;
+  let dividend = 0;
+
+  if (!numerator1) {
+    numerator1 = 0;
+  }
+
+  if (wholeNum1 == 0) {
+    dividend = interval * numerator1;
+  } else {
+    dividend = interval * wholeNum1 + (interval / denominator1) * numerator1;
+  } // unit: canvas pixels
+
+  // Removing this code from here for now to put it closer to the end so these lines display above the colored sections. I'm leaving here because I may add it back later if I want to have an animation that shows the original dividend and then counts by divisors one at a time.
+
+  //   canvas.beginPath();
+  //   canvas.fillStyle = "gray";
+  //   canvas.fillRect(
+  //     lineThickness,
+  //     lineThickness,
+  //     dividend - lineThickness * 2,
+  //     height - lineThickness * 2
+  //   );
+  //   canvas.closePath();
+  //   canvas.stroke();
+
+  //   for (let i = 1; i < numSections; i++) {
+  //     canvas.beginPath();
+  //     canvas.moveTo(separator, 0);
+  //     canvas.lineTo(separator, height);
+  //     canvas.closePath();
+  //     canvas.lineWidth = lineThickness;
+  //     canvas.stroke();
+  //     separator = separator + interval;
+  //   }
+
+  let segmenter; // This represents how big the divisor is in pixels relative to the divident
+  if (wholeNum1 == 0) {
+    segmenter = (width / denominator2) * numerator2;
+  } else {
+    segmenter = (interval / denominator2) * numerator2;
+  }
+
+  let numSegments = ((wholeNum1 + 1) * denominator2) / numerator2 + 1;
+
+  //color each section alternating colors
+  for (let i = 1; i <= numSegments; i++) {
+    if (i % 2 == 0) {
+      canvas.fillStyle = colorFill2;
+      canvas.fillRect(segmenter * i - segmenter, 0, segmenter * i, height);
+    } else {
+      canvas.fillStyle = colorFill;
+      canvas.fillRect(segmenter * i - segmenter, 0, segmenter * i, height);
     }
-canvas.clearRect(0, 0, width, height);
-    const interval =  width/ denominator;
-    let separator = interval; // Need two variables becuase separator will increment while interval stays constant. 
 
-    let shaded = interval * numerator;
-    canvas.fillStyle = "black";
-    canvas.fillRect(0, 0, width, height);
     canvas.fillStyle = "white";
-    canvas.fillRect(lineThickness, lineThickness, width -  (lineThickness*2), height - (lineThickness*2));
-    canvas.fillStyle = colorFill;
-    canvas.fillRect(lineThickness, lineThickness, shaded - lineThickness, height - lineThickness);
+    canvas.fillRect(dividend, 0, width, height);
 
-    for (let i = 1; i < denominator; i++) {
-        canvas.beginPath();
-        canvas.moveTo(separator, 0);
-        canvas.lineTo(separator, height);
-        canvas.closePath();
-        canvas.lineWidth = lineThickness;
-        canvas.stroke();
-        separator = separator + interval;
-    }
-}
+    // Dashed border which is invisible if the dividend is a whole number but shows the next integer up if the dividend is a mixed number
+    canvas.fillStyle = "black";
+    canvas.setLineDash([5, 6]);
+    canvas.lineWidth = lineThickness * 2;
+    canvas.beginPath();
+    canvas.moveTo(lineThickness, lineThickness);
+    canvas.lineTo(lineThickness, height + lineThickness);
+    canvas.lineTo(width - lineThickness, height + lineThickness);
+    canvas.lineTo(width - lineThickness, lineThickness);
+    canvas.lineTo(lineThickness, lineThickness);
+    canvas.stroke();
 
-mathVisual.fractionCircle = (canvasElement, numerator, denominator, lineThickness=5, colorFill="rgb(120, 190, 250)") => {
-
-    const width = canvasElement.width;
-    const height = canvasElement.height;
-    if (numerator < 0 || denominator < 0) {
-        return alert(
-            `Please enter a positive numerator and denominator.`
-        );
-}
-   
-    if (numerator > denominator) {
-        return alert(
-            `Please enter a proper fraction. num=${numerator} denom=${denominator} ${numerator > denominator}`
-        );
-    }
-
-    let canvas = canvasElement.getContext("2d");
-    canvas.clearRect(0, 0, width, height);
-
-
-// Set the circle properties
-const x = width/2; // x-coordinate of the center
-const y = height/2; // y-coordinate of the center
-let radius = 0;
-if (width <= height) { 
- radius = width / 2 - 10;
-}
-else {  radius = height/2 - 10; }
-const startAngle = 0; // starting angle (in radians)
-const endAngle = 2 * Math.PI; // ending angle (in radians)
-
-const angle = (Math.PI * 2) / denominator;
-let currentAngle = angle;
-let shaded = angle * numerator;
-
-canvas.beginPath();
-canvas.lineWidth = lineThickness;
-canvas.arc(x,y,radius,startAngle, endAngle); // Drawing the outer circle
-canvas.fillStyle = "white";
-canvas.fill();
-canvas.lineTo(x,y);
-canvas.stroke();
-
-canvas.beginPath(); // Filling in the shaded pieces
-canvas.arc(x,y, radius, startAngle, shaded);
-canvas.lineTo(x,y);
-// I've only drawn an arc and one line, but when the fill() method is called, the canvas will automatically draw a line back to the starting point. So I do not need to explicitly create the 3rd line to close the path to be filled. 
-canvas.fillStyle = colorFill;
-canvas.fill();
-canvas.stroke();
-
-canvas.beginPath();
-for (i=0; i<denominator; i++) { // Creating lines for each slice
-    canvas.arc(x,y, radius, currentAngle, currentAngle+angle);
-    canvas.lineTo(x,y);
-    currentAngle = currentAngle+angle;
-}
-canvas.stroke();
-}
-
-mathVisual.fractionMultiplication = 
-(canvasElement,  wholeNum1, numerator1, denominator1, wholeNum2, numerator2, denominator2, style="area") => {
-    let canvas = canvasElement.getContext("2d");
-    const width = canvasElement.width;
-    const height = canvasElement.height;
-    canvas.clearRect(0, 0, width, height);
-
-
-
-    if (wholeNum1 === 0 && wholeNum2 === 0) {
-
-    }
-
-}
-
-mathVisual.fractionDivision = 
-(canvasElement, wholeNum1, numerator1, denominator1, numerator2, denominator2, lineThickness=4, colorFill="#337782", colorFill2 = "#f0a68c", style="bar") => {
-    let canvas = canvasElement.getContext("2d");
-    const width = canvasElement.width;
-    const spaceForLabels = 50 
-    const height = canvasElement.height - spaceForLabels;
-    canvas.clearRect(0, 0, width, height+spaceForLabels);
-    let numSections;
-
-
-    // Need to add error handling for decimal inputs or blank inputs
-
-    if (!wholeNum1) { wholeNum1 = 0 }; // If the wholeNum input is blank, the variable is set to 0. 
-
-    if (wholeNum1 < 0 || numerator1 < 0 || denominator1 <0 || numerator2 <0 || denominator2<0) { 
-        return alert(
-            `Please enter a positive number in each box or leave it blank.`
-        );
-    }
-    else if (wholeNum1 == 0 && numerator1 ==0) {
-        return alert(
-            `Please enter a positive dividend.`
-        );
-    }
-    else if (denominator1 == 0 || denominator2 == 0) 
-    {
-        return alert(
-            `All denominators must be positive.`
-        );
-    }
-    else if (wholeNum1 == 0) { numSections = denominator1 }
-    else if (numerator1 == 0) { numSections = wholeNum1 }
-    else { numSections = wholeNum1 +1 } 
-
-    const interval = width / numSections;
-    let separator = interval;
-    let dividend = 0;
-
-    if (!numerator1) {numerator1 = 0};
-
-    if (wholeNum1 == 0) { dividend = interval * numerator1 }
-    else { dividend = interval* wholeNum1 + ((interval / denominator1)*numerator1) } // unit: canvas pixels
-
-
-
-// Removing this code from here for now to put it closer to the end so these lines display above the colored sections. I'm leaving here because I may add it back later if I want to have an animation that shows the original dividend and then counts by divisors one at a time. 
-
-    // canvas.beginPath();
+    // Create dashed lines to segment each iteration of the divisor
+    canvas.setLineDash([5, 10]);
     // canvas.fillStyle = colorFill;
-    // canvas.fillRect(lineThickness, lineThickness, dividend - lineThickness*2, height - lineThickness*2);
-    // canvas.closePath();
-    // canvas.stroke();
+    canvas.lineWidth = lineThickness + 1;
+    canvas.beginPath();
+    canvas.moveTo(segmenter * i, 0);
+    canvas.lineTo(segmenter * i, height);
+    canvas.closePath();
+    canvas.stroke();
+  }
 
-    // for (let i = 1; i < numSections; i++) {
-    //     canvas.beginPath();
-    //     canvas.moveTo(separator, 0);
-    //     canvas.lineTo(separator, height);
-    //     canvas.closePath();
-    //     canvas.lineWidth = lineThickness;
-    //     canvas.stroke();
-    //     separator = separator + interval;
-    // }
-
-let segmenter; // This represents how big the divisor is in pixels relative to the divident
-if (wholeNum1 == 0) { segmenter = (width / denominator2)*numerator2 }
-else { segmenter = (interval / denominator2) * numerator2 }; 
-
-let numSegments = (wholeNum1+1) * denominator2 / numerator2+1;
-
-
-//color each section alternating colors
-    for (let i=1; i<=numSegments; i++) {
-        if (i%2 == 0) {
-            canvas.fillStyle = colorFill2;
-            canvas.fillRect(segmenter*i-segmenter, 0, segmenter*i, height);
-        }
-        else { 
-            canvas.fillStyle = colorFill;
-            canvas.fillRect(segmenter*i-segmenter, 0, segmenter*i, height);
-        }
-
-
-        canvas.fillStyle = "white";
-        canvas.fillRect(dividend, 0, width, height);
-
-        // Dashed border which is invisible if the dividend is a whole number but shows the next integer up if the dividend is a mixed number
-        canvas.fillStyle = "black";
-        canvas.setLineDash([5, 6]);
-        canvas.lineWidth = lineThickness*2;
-        canvas.beginPath();
-        canvas.moveTo(lineThickness,lineThickness);
-        canvas.lineTo(lineThickness, height+lineThickness);
-        canvas.lineTo(width- lineThickness, height+lineThickness);
-        canvas.lineTo(width- lineThickness, lineThickness);
-        canvas.lineTo(lineThickness,lineThickness);
-        canvas.stroke();
-
-        // Create dashed lines to segment each iteration of the divisor
-        canvas.setLineDash([5, 10]);
-        // canvas.fillStyle = colorFill;
-        canvas.lineWidth = lineThickness;
-        canvas.beginPath();
-        canvas.moveTo(segmenter*i, 0);
-        canvas.lineTo(segmenter*i, height);
-        canvas.closePath();
-        canvas.stroke();
+  canvas.setLineDash([]);
+  // Create solid separators for each whole OR if the dividend is less than one, for each portion
+  for (let i = 1; i < numSections; i++) {
+    canvas.beginPath();
+    canvas.moveTo(separator, 0);
+    canvas.lineTo(separator, height);
+    canvas.closePath();
+    if (wholeNum1 != 0) {
+      canvas.lineWidth = lineThickness * 2;
+    } else {
+      canvas.lineWidth = lineThickness;
     }
+    canvas.stroke();
+    separator = separator + interval;
+  }
 
-    canvas.setLineDash([]);
-// Create solid separators for each whole OR if the dividend is less than one, for each portion
-    for (let i = 1; i < numSections; i++) {
-        canvas.beginPath();
-        canvas.moveTo(separator, 0);
-        canvas.lineTo(separator, height);
-        canvas.closePath();
-        if (wholeNum1 != 0) {canvas.lineWidth = lineThickness*2}
-        else { canvas.lineWidth = lineThickness }
-        canvas.stroke();
-        separator = separator + interval;
-    }
+  // thin unit fraction lines
+  let unitFractionWidth;
+  if (wholeNum1 == 0) {
+    unitFractionWidth = width / denominator2;
+  } else {
+    unitFractionWidth = width / (numSections * denominator2);
+  }
+  let unitFracDivider = unitFractionWidth;
+  canvas.setLineDash([]);
+  canvas.strokeStyle = "black";
+  for (let i = 1; i < numSections * denominator2; i++) {
+    canvas.lineWidth = lineThickness / 4;
+    canvas.beginPath();
+    canvas.moveTo(unitFracDivider, lineThickness);
+    canvas.lineTo(unitFracDivider, height);
+    canvas.stroke();
+    unitFracDivider = unitFracDivider + unitFractionWidth;
+  }
 
-      // thin unit fraction lines
-    let unitFractionWidth;
-    if (wholeNum1 == 0) {  unitFractionWidth = width / denominator2 } 
-    else { unitFractionWidth = width / (numSections * denominator2) }
-    let unitFracDivider = unitFractionWidth;
-    canvas.setLineDash([]);
-    canvas.strokeStyle = "black";
-    for (let i=1; i<numSections*denominator2; i++) {
-        canvas.lineWidth = lineThickness /4;
-        canvas.beginPath();
-       canvas.moveTo(unitFracDivider, lineThickness);
-       canvas.lineTo(unitFracDivider, height);
-       canvas.stroke();
-       unitFracDivider = unitFracDivider + unitFractionWidth;
-    } 
-
-
-    // Create solid black outline
-    canvas.setLineDash([]); // Resets the line to solid instead of dashed
-    canvas.strokeStyle = "black";
-    canvas.lineWidth = lineThickness*2;
-    canvas.strokeRect(lineThickness, lineThickness, dividend, height);
-
-}
+  // Create solid black outline
+  canvas.setLineDash([]); // Resets the line to solid instead of dashed
+  canvas.strokeStyle = "black";
+  canvas.lineWidth = lineThickness * 2;
+  canvas.strokeRect(lineThickness, lineThickness, dividend, height);
+};
--- script.js
+++ script.js
@@ -1,74 +1,131 @@
 // Global Functions
 
 function downloadPng(canvasEl) {
-    const canvas = document.getElementById(canvasEl) // Gets canvas element from html
-    const link = document.createElement('a');
-    link.href = canvas.toDataURL("image/png");
-    link.download = 'canvas_image.png';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  const canvas = document.getElementById(canvasEl); // Gets canvas element from html
+  const link = document.createElement("a");
+  link.href = canvas.toDataURL("image/png");
+  link.download = "canvas_image.png";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 }
 
 function basicFrac() {
+  // HTML Elements from the basic fraction page
+  const generateBasicButton = document.getElementById("generate-basic");
+  const modelToggle = document.getElementById("basic-model-toggle");
+  const basicCanvas = document.getElementById("basic-canvas");
 
-    // HTML Elements from the basic fraction page
-    const generateBasicButton = (document.getElementById("generate-basic"));
+  generateBasicButton.addEventListener("click", function () {
+    const numerator = parseInt(
+      document.getElementById("basic-numerator").value
+    );
+    const denominator = parseInt(
+      document.getElementById("basic-denominator").value
+    );
 
-    generateBasicButton.addEventListener("click", function () {
-        const basicCanvas = document.getElementById("basic-canvas");
-        const numerator = parseInt(document.getElementById("basic-numerator").value);
-        const denominator= parseInt(document.getElementById("basic-denominator").value);
-        mathVisual.fractionCircle(basicCanvas, numerator, denominator);
-    });
+    if (modelToggle.checked) {
+      mathVisual.fractionBar(basicCanvas, numerator, denominator);
+    } else {
+      mathVisual.fractionCircle(basicCanvas, numerator, denominator);
+    }
+  });
 
-    const basicPngButton = document.getElementById("basic-png-button");
-    basicPngButton.addEventListener("click", function () {
-        downloadPng("basic-canvas");
-    })
+  modelToggle.addEventListener("change", () => {
+    const numerator = parseInt(
+      document.getElementById("basic-numerator").value
+    );
+    const denominator = parseInt(
+      document.getElementById("basic-denominator").value
+    );
+    if (modelToggle.checked) {
+      mathVisual.fractionBar(basicCanvas, numerator, denominator);
+    } else {
+      mathVisual.fractionCircle(basicCanvas, numerator, denominator);
+    }
+  });
 
+  const basicPngButton = document.getElementById("basic-png-button");
+  basicPngButton.addEventListener("click", function () {
+    downloadPng("basic-canvas");
+  });
 }
 
 function multiplicationFrac() {
+  // HTML Elements from the Multiplication page
+  const generateMultiplicationButton = document.getElementById(
+    "generate-multiplication"
+  );
 
-// HTML Elements from the Multiplication page
-const generateMultiplicationButton = document.getElementById("generate-multiplication");
+  generateMultiplicationButton.addEventListener("click", function () {
+    const multiplicationCanvas = document.getElementById(
+      "multiplication-canvas"
+    );
+    const multWholeNum1 = parseInt(
+      document.getElementById("multiplication-whole-number1").value
+    );
+    const multNumerator1 = parseInt(
+      document.getElementById("multiplication-numerator1").value
+    );
+    const multDenominator1 = parseInt(
+      document.getElementById("multiplication-denominator1").value
+    );
+    const multWholeNum2 = parseInt(
+      document.getElementById("multiplication-whole-number2").value
+    );
+    const multNumerator2 = parseInt(
+      document.getElementById("multiplication-numerator2").value
+    );
+    const multDenominator2 = parseInt(
+      document.getElementById("multiplication-denominator2").value
+    );
 
-generateMultiplicationButton.addEventListener("click", function () {
-    const multiplicationCanvas = document.getElementById("multiplication-canvas");
-    const multWholeNum1 = parseInt(document.getElementById("multiplication-whole-number1").value);
-    const multNumerator1 = parseInt(document.getElementById("multiplication-numerator1").value);
-    const multDenominator1 = parseInt(document.getElementById("multiplication-denominator1").value);
-    const multWholeNum2 = parseInt(document.getElementById("multiplication-whole-number2").value);
-    const multNumerator2 = parseInt(document.getElementById("multiplication-numerator2").value);
-    const multDenominator2 = parseInt (document.getElementById("multiplication-denominator2").value);
-
-    mathVisual.fractionMultiplication(multiplicationCanvas, multWholeNum1, multNumerator1, multDenominator1, multWholeNum2, multNumerator2, multDenominator2);
-});
-
+    mathVisual.fractionMultiplication(
+      multiplicationCanvas,
+      multWholeNum1,
+      multNumerator1,
+      multDenominator1,
+      multWholeNum2,
+      multNumerator2,
+      multDenominator2
+    );
+  });
 }
 
 function divisionFrac() {
-// HTML Elements from the Division page
-const generateDivisionButton = document.getElementById("generate-division");
+  // HTML Elements from the Division page
+  const generateDivisionButton = document.getElementById("generate-division");
 
-generateDivisionButton.addEventListener("click", function () {
+  generateDivisionButton.addEventListener("click", function () {
     const divisionCanvas = document.getElementById("division-canvas");
-    const divisionWholeNum1 = parseInt(document.getElementById("division-whole-number1").value);
-    const divisionNumerator1 = parseInt(document.getElementById("division-numerator1").value);
-    const divisionDenominator1 = parseInt(document.getElementById("division-denominator1").value);
-    const divisionNumerator2 = parseInt(document.getElementById("division-numerator2").value);
-    const divisionDenominator2 = parseInt (document.getElementById("division-denominator2").value);
+    const divisionWholeNum1 = parseInt(
+      document.getElementById("division-whole-number1").value
+    );
+    const divisionNumerator1 = parseInt(
+      document.getElementById("division-numerator1").value
+    );
+    const divisionDenominator1 = parseInt(
+      document.getElementById("division-denominator1").value
+    );
+    const divisionNumerator2 = parseInt(
+      document.getElementById("division-numerator2").value
+    );
+    const divisionDenominator2 = parseInt(
+      document.getElementById("division-denominator2").value
+    );
 
-    mathVisual.fractionDivision(divisionCanvas, divisionWholeNum1, divisionNumerator1, divisionDenominator1, divisionNumerator2, divisionDenominator2);
-});
+    mathVisual.fractionDivision(
+      divisionCanvas,
+      divisionWholeNum1,
+      divisionNumerator1,
+      divisionDenominator1,
+      divisionNumerator2,
+      divisionDenominator2
+    );
+  });
 
-const divisionPngButton = document.getElementById("division-png-button");
-divisionPngButton.addEventListener("click", function () {
+  const divisionPngButton = document.getElementById("division-png-button");
+  divisionPngButton.addEventListener("click", function () {
     downloadPng("division-canvas");
-})
-
+  });
 }
-
-
-
